fc53133e2cf6eb0b12e3e3f652dff23afd6ad7f9
Joe Susnick <jsusnick@itriagehealth.com>
2014-11-01 15:22:18 -0600

    Assets for the initial app

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Joe Susnick on 11/1/14.
+//  Copyright (c) 2014 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ribbit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Joe Susnick on 11/1/14.
+//  Copyright (c) 2014 Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
